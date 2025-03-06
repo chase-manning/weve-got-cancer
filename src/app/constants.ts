6c2bf1185f8063bd3d630a380303b169f3338bbe
Chase Manning <chase@manning.dev>
2025-03-06 16:25:24 +1300

    :sparkles: add author link

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,4 @@
 export const AMAZON_LINK =
   "https://www.amazon.com.au/Weve-Got-Cancer-challenging-diagnosis-ebook/dp/B08R9MFXCQ";
+export const AUTHOR_LINK =
+  "https://www.amazon.com.au/stores/Kushla-Rolls/author/B08RCB6HP9";
