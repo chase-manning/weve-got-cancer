9975dd31ef9353cbf67685a39ae79d4324003843
Chase Manning <chase@manning.dev>
2025-03-16 21:28:06 +1300

    :wrench: update amazon links

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
 export const AMAZON_LINK =
-  "https://www.amazon.com.au/Weve-Got-Cancer-challenging-diagnosis-ebook/dp/B08R9MFXCQ";
+  "https://www.amazon.com/Weve-Got-Cancer-challenging-diagnosis-ebook/dp/B08R9MFXCQ";
 export const AUTHOR_LINK =
-  "https://www.amazon.com.au/stores/Kushla-Rolls/author/B08RCB6HP9";
+  "https://www.amazon.com/stores/Kushla-Rolls/author/B08RCB6HP9";
