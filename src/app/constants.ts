544caa41f372941aa24da5135bee840d559cccb1
Chase Manning <chase@manning.dev>
2025-05-11 14:16:26 +0100

    :wrench: update book linker link

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,3 @@
-export const AMAZON_LINK =
-  "https://www.amazon.com/Weve-Got-Cancer-challenging-diagnosis-ebook/dp/B08R9MFXCQ";
+export const AMAZON_LINK = "https://mybook.to/weve-got-cancer";
 export const AUTHOR_LINK =
   "https://www.amazon.com/stores/Kushla-Rolls/author/B08RCB6HP9";
